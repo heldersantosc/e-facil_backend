2edd79522ca575ff79c940b2cd0da2e95116919a
Helder Santos <heldersantos.c@gmail.com>
2020-05-19 23:59:26 -0400

    novos ajustes
    Signed-off-by: Helder Santos <heldersantos.c@gmail.com>

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.clear();
-  console.log("e-Facil on server port: " + port);
+  console.log("e-Facil on server port: " + PORT);
 });
